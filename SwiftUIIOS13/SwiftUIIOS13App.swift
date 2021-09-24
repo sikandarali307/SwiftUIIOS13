abbd339def379a8597967adef2ed1e4979931e64
sikandar <sikandar.ali307@gmail.com>
2021-09-24 12:47:02 +0500

    Initial Commit

---
+++ SwiftUIIOS13/SwiftUIIOS13App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIOS13App.swift
+//  SwiftUIIOS13
+//
+//  Created by Sikandar Ali on 24/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIOS13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
